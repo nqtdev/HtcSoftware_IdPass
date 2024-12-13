eb098d59772aacabfb3f473a90aa427724e0fa5a
nqtien <nguyenquoctien2000@gmail.com>
2024-12-13 13:58:58 +0700

    Update: Add certificate

--- src/pages/IdPass/index.jsx
+++ src/pages/IdPass/index.jsx
@@ -1,18 +1,28 @@
 import Field from './field'
-import PosterPage from './poster'
+import PosterPage from './Poster'
 import Procedure from './procedure'
-
+import Certificate from './Certificate'
+import Feature from './Feature'
 const IdPassPage = () => {
   return (
     <div className='container m-auto'>
       <section>
         <PosterPage />
       </section>
+      <section>
+        <Feature />
+      </section>
       <section className='h-[38rem]'>
         <div className='m-10'>
           <h2 className='base_title '>Giải pháp tự động hoá thủ tục kiểm tra thông tin</h2>
         </div>
         <Procedure />
+      </section>
+      <section>
+        <div className='m-10'>
+          <h2 className='base_title'>Giấy chứng nhận</h2>
+        </div>
+        <Certificate />
       </section>
       <section>
         <div className='m-10'>
